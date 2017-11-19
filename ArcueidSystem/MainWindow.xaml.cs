e01fb25adde8779f29ad81be7a16f288a760ec67
SuzukageHotori <Taburiss@Taburiss.com>
2017-11-19 11:07:49 +0800

    FIRST SUMMIT

---
+++ ArcueidSystem/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArcueidSystem
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpServer http = new HttpServer(new Server(8081));
+            http.Handlers.Add(new SubstitutingFileReader());
+            http.Handlers.Add(new RESTfulApiHandlerBase(HMethod.POST, "/api/hello", new List<string>() {"index","userid" }, new HelloHander()));
+            this.IsEnabled = false;
+        }
+        public class HelloHander : IRESTfulHandler
+        {
+
+            public bool Process(HttpServer server, HttpRequest request, HttpResponse response, Dictionary<string, string> param)
+            {
+                
+                response.Content = " "+param["index"];
+                return true;
+            }
+        }
+
+    }
+}
